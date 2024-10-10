5a1c98e3d7ec0537333d1e8455959268e392e2b7
Alone2Me <bros309@outlook.com>
2024-10-10 17:07:28 +0700

    edit-index-category-page

--- resources/js/Pages/Categories/Index.vue
+++ resources/js/Pages/Categories/Index.vue
@@ -2,22 +2,15 @@
 import { onBeforeMount, ref } from "vue";
 
 import AppLayout from "@/Layouts/AppLayout.vue";
-import { CustomerService } from "@/service/CustomerService";
-import { ProductService } from "@/service/ProductService";
-import { FilterMatchMode, FilterOperator } from "@primevue/core/api";
+
 
 defineProps({
   categories: Object,
   product: Object,
 });
 
-const customers1 = ref(null);
-const customers2 = ref(null);
-const customers3 = ref(null);
-const filters1 = ref(null);
-const loading1 = ref(null);
 const products = ref(null);
-const expandedRows = ref([]);
+
 function getOrderSeverity(order) {
   switch (order.status) {
     case "DELIVERED":
@@ -45,51 +38,7 @@ function getStockSeverity(product) {
       return null;
   }
 }
-onBeforeMount(() => {
-  ProductService.getProductsWithOrdersSmall().then((data) => (products.value = data));
-  CustomerService.getCustomersLarge().then((data) => {
-    customers1.value = data;
-    loading1.value = false;
-    customers1.value.forEach((customer) => (customer.date = new Date(customer.date)));
-  });
-  CustomerService.getCustomersLarge().then((data) => (customers2.value = data));
-  CustomerService.getCustomersMedium().then((data) => (customers3.value = data));
-  initFilters1();
-});
-function initFilters1() {
-  filters1.value = {
-    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-    name: {
-      operator: FilterOperator.AND,
-      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-    },
-    "country.name": {
-      operator: FilterOperator.AND,
-      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-    },
-    representative: { value: null, matchMode: FilterMatchMode.IN },
-    date: {
-      operator: FilterOperator.AND,
-      constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }],
-    },
-    balance: {
-      operator: FilterOperator.AND,
-      constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
-    },
-    status: {
-      operator: FilterOperator.OR,
-      constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
-    },
-    activity: { value: [0, 100], matchMode: FilterMatchMode.BETWEEN },
-    verified: { value: null, matchMode: FilterMatchMode.EQUALS },
-  };
-}
-function expandAll() {
-  expandedRows.value = products.value.reduce((acc, p) => (acc[p.id] = true) && acc, {});
-}
-function collapseAll() {
-  expandedRows.value = null;
-}
+
 function formatCurrency(value) {
   return value.toLocaleString("en-US", {
     style: "currency",
@@ -118,8 +67,8 @@ function formatCurrency(value) {
           </template>
         </Column>
         <Column
-          field="product"
-          header="Name"
+          field="name"
+          header="Product"
           :sortable="true"
           style="width: 35%"
         ></Column>
